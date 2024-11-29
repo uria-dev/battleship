b774ec7657d6fab50cdb27755b6b1ce1d8af39bf
Uria <uria@urialevy.com>
2024-11-29 00:01:49 +0000

    receive attack and check for game over tests pass

--- gameBoard.js
+++ gameBoard.js
@@ -13,6 +13,7 @@ class gameBoard {
     this.height = h;
     this.board;
     this.build(w, h);
+    this.ships = [];
   }
   build(w, h) {
     let arr = [];
@@ -35,21 +36,42 @@ class gameBoard {
     if (this.board[x][y].ship) {
       throw `Ships cannot overlap`;
     }
-
+    this.ships.push(ship);
     for (let i = 0; i < ship.len; i++) {
       this.board[x][y].ship = ship;
       this.board[x][y].hasShip = true;
       ship.orientation == "h" ? (y += 1) : (x += 1);
     }
+
     return true;
   }
   receiveAttack([x, y]) {
-    if (x < 0 || y < 0) {
-      throw `cannot target negative coordinates`;
+    if (x < 0 || y < 0 || x > 10 || y > 10) {
+      throw `Illegal targeting coordinates`;
+    }
+    if (this.board[x][y].hit) {
+      return `Already hit that spot!`;
     }
     if (this.board[x][y].hasShip == false) {
       this.board[x][y].hit = true;
+      return `Miss!`;
     }
+    if (this.board[x][y].hasShip == true) {
+      this.board[x][y].hit = true;
+      this.board[x][y].ship.hit();
+      if (this.checkGameOver()) {
+        return `All ships sunk!`;
+      }
+      return true;
+    }
+  }
+  checkGameOver() {
+    for (let i = 0; i < this.ships.length; i++) {
+      if (this.ships[i].sunk == false) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
--- gameBoard.test.js
+++ gameBoard.test.js
@@ -11,12 +11,6 @@ test("Cannot exceed board limt", () => {
   }).toThrow(`Cannot exceed board limits`);
 });
 
-test("cannot target negative coordinates", () => {
-  expect(() => {
-    gb.receiveAttack([-2, 0]);
-  }).toThrow(`cannot target negative coordinates`);
-});
-
 test("ship is placed on board exists only in one spot", () => {
   gb.placeShip([0, 0], new Ship(3, "h"));
   expect(gb.board[0][0].ship).not.toBeUndefined();
@@ -31,4 +25,41 @@ test("ship is placed on board exists only in one spot", () => {
   expect(() => {
     gb.placeShip([0, 0], new Ship(1));
   }).toThrow(`Ships cannot overlap`);
+});
+
+test("Missing", () => {
+  expect(gb.receiveAttack([9, 9])).toBe(`Miss!`);
+});
+test("Hitting a ship in a spot that hasn't been hit yet", () => {
+  gb.receiveAttack([0, 0]);
+  expect(gb.board[0][0].ship.hits).toBe(1);
+  gb.receiveAttack([0, 1]);
+  expect(gb.board[0][1].ship.hits).toBe(2);
+});
+test("Sinking a ship", () => {
+  expect(gb.receiveAttack([0, 2])).toBe(true);
+});
+test("Hitting a spot that was already hit", () => {
+  expect(gb.receiveAttack([0, 1])).toBe(`Already hit that spot!`);
+  expect(gb.receiveAttack([9, 9])).toBe(`Already hit that spot!`);
+});
+test("cannot target illegal coordinates", () => {
+  expect(() => {
+    gb.receiveAttack([-10, 1]);
+  }).toThrow(`Illegal targeting coordinates`);
+  expect(() => {
+    gb.receiveAttack([11, 1]);
+  }).toThrow(`Illegal targeting coordinates`);
+  expect(() => {
+    gb.receiveAttack([7, -5]);
+  }).toThrow(`Illegal targeting coordinates`);
+  expect(() => {
+    gb.receiveAttack([7, -12]);
+  }).toThrow(`Illegal targeting coordinates`);
+});
+
+test("check for a game over", () => {
+  expect(gb.receiveAttack([0, 5])).not.toBe(`All ships sunk!`);
+  expect(gb.receiveAttack([1, 5])).not.toBe(`All ships sunk!`);
+  expect(gb.receiveAttack([2, 5])).toBe(`All ships sunk!`);
 });
