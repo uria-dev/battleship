5a343c4d987f24ba9cecbdae44104d15cfafcc3e
Uria <uria@urialevy.com>
2025-03-31 22:47:08 +0100

    remove unneeded import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import { expect, jest, test } from "@jest/globals";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ];
