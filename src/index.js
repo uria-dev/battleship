448cc2b647f36fe157cfe3628f700109ff3c507e
Uria <uria@urialevy.com>
2025-04-06 23:28:36 +0100

    initial loading capability

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
+import gameBoard from "./gameBoard.js";
+import Ship from "./ship.js";
 import "./style.css";
 
 const Player = require("./player.js");
 const initialHTML = `
-<body>
-  <header>
-    <h1>Battleship Game</h1>
-  </header>
   <main>
     <div class="game-container">
       <form id="player-form">
@@ -22,23 +20,37 @@ const initialHTML = `
             </select>
           </div>
         </div>
-        <button id="start-game">Start Game</button>
+        <button id="start-game" action="submit">Start Game</button>
       </form>
     </div>
-  </main>
-</body>`;
-const bodyHTML = `  <body>
+  </main>`;
+
+const body = document.querySelector("body");
+body.innerHTML = initialHTML;
+
+const startGameButton = document.querySelector("#start-game");
+const startGameForm = document.querySelector("#player-form");
+const player1NameInput = document.querySelector("#player1-name");
+const player2NameInput = document.querySelector("#player2-name");
+const toggleComputerInput = document.querySelector("#toggle-computer");
+
+startGameForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const toggleComputer = toggleComputerInput.checked;
+  const player1Name = player1NameInput.value;
+  const player2Name = toggleComputer ? "🤖" : player2NameInput.value;
+  const gameHTML = `  <body>
     <header>
       <h1>Battleship Game</h1>
     </header>
     <main>
       <div class="game-container">
         <div class="player-board">
-          <h2>Player 1</h2>
+          <h2> Admiral ${player1Name}</h2>
           <div class="grid" id="player1-grid"></div>
         </div>
         <div class="player-board">
-          <h2>Player 2 / Computer</h2>
+          <h2>Admiral ${player2Name}</h2>
           <div class="grid" id="player2-grid"></div>
         </div>
       </div>
@@ -46,12 +58,21 @@ const bodyHTML = `  <body>
       </div>
     </main>
   </body>`;
-const body = document.querySelector("body");
-body.innerHTML = initialHTML;
-const player1Grid = document.querySelector("#player1-grid");
-const player2Grid = document.querySelector("#player2-grid");
-const startGameButton = document.querySelector("#start-game");
-const resetGameButton = document.querySelector("#reset-game");
+
+  body.innerHTML = gameHTML;
+  const player1Grid = document.querySelector("#player1-grid");
+  const player2Grid = document.querySelector("#player2-grid");
+  createCells(player1Grid);
+  createCells(player2Grid);
+  startGame(player1Name, player2Name, toggleComputer);
+});
+
+function startGame(player1Name, player2Name, toggleComputer) {
+  const player1 = new Player(player1Name, true);
+  const player2 = new Player(player2Name, !toggleComputer);
+  placeShips(player1);
+  placeShips(player2);
+}
 
 function createCells(grid) {
   for (let i = 0; i < 10; i++) {
@@ -65,5 +86,12 @@ function createCells(grid) {
   }
 }
 
-createCells(player1Grid);
-createCells(player2Grid);
+function placeShips(player) {
+  let shipArray = [];
+  shipArray.push(new Ship(5, "Carrier", "h"));
+  shipArray.push(new Ship(4, "Battleship", "h"));
+  shipArray.push(new Ship(3, "Cruiser", "h"));
+  shipArray.push(new Ship(3, "Submarine", "h"));
+  shipArray.push(new Ship(2, "Destroyer", "h"));
+  player.ships = shipArray;
+}
